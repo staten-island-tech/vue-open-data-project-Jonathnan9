1c15bb98d39373f187326cbf576ab0834efa4986
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-03-17 12:32:18 -0400

    eae

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,17 +2,19 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
 import GetData from './components/GetData.vue'
+import DisplayCharts from './components/DisplayCharts.vue'
 </script>
 
 <template>
   <div>
     <NavBar />
     <GetData />
+    <DisplayCharts />
 
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
     <div class="min-h-screen flex flex-col pt-16">
-      <div class="flex-1 p-4" data-theme="emerald">
+      <div class="flex-1 p-4">
         <RouterView />
       </div>
     </div>
--- app/src/components/GetData.vue
+++ app/src/components/GetData.vue
@@ -1,16 +1,15 @@
 <template>
-  <div>
-    <h1>{{ thing.value }}</h1>
-  </div>
+  <div></div>
 </template>
 
 <script setup>
+//https://data.cityofnewyork.us/Public-Safety/NYPD-Shooting-Incident-Data-Year-To-Date-/5ucz-vwe8/data_preview
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const thing = ref('')
 async function getData() {
-  let res = await fetch(`https://data.cityofnewyork.us/resource/rvmf-4sg6.json`)
+  let res = await fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json`)
   let data = await res.json()
   thing.value = data
   console.log(thing)
